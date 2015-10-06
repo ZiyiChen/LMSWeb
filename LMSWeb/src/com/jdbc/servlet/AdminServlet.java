ae33cbf2ce64c49b1ec3c317f8d4fc3774264e32
ZiyiChen <chen216@mail.chapman.edu>
2015-10-06 03:03:08 -0400

    Testing with doPost function

--- LMSWeb/src/com/jdbc/servlet/AdminServlet.java
+++ LMSWeb/src/com/jdbc/servlet/AdminServlet.java
@@ -1,11 +1,16 @@
 package com.jdbc.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.jdbc.lmdo.Author;
+import com.jdbc.lmsys.AdministratorManagementSys;
 
 /**
  * Servlet implementation class AdminServlet
@@ -32,8 +37,19 @@ public class AdminServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String authorName = request.getParameter("authorName");
-		System.out.println(authorName);
+		System.out.println("HERE1");
+		try {
+			String action = request.getParameter("action");
+			System.out.println("HERE: "+ action);
+			if (action.equals("addAuthor")) {
+				String authorName = request.getParameter("authorName");
+				Author author = new Author();
+				author.setAuthorName(authorName);
+				new AdministratorManagementSys().insertAuthor(author);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
